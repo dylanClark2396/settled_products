f6fb03bb6f4d4daed272e5bd5fbc3de8a95868a1
dylanClark2396 <dylanclark2396@gmail.com>
2025-03-04 14:50:38 -0600

    changed to dist

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,29 @@
 <template>
   <!-- whole page -->
-  <div style="display: flex; align-items: center; justify-content: center; flex-flow: column;">
-    <!-- filters -->
-    <div style="display: flex;">
-      left
-    </div>
-    <!-- product list -->
-    <div style="display: flex;">
-      right
+  <div style="display: flex; align-items: center; justify-content: center; flex-flow: column; height: 100vh;">
+    <div class="give-border" style="display: flex; width: 100%; height: 100%;">
+      <!-- filters -->
+      <div class="give-border" style="width: 10%; margin: 1rem">
+        left
+      </div>
+      <!-- product list -->
+      <div class="give-border" style="width: 90%; margin: 1rem">
+        right
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="css">
+.give-border {
+  border: 2px dotted rgb(96 139 168);
+}
+
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: fit-content;
+  width: fit-content;
+}
+</style>
