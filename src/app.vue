f35579a871358eb4fea429621114dea69b8d9c70
dylanClark2396 <dylanclark2396@gmail.com>
2025-03-04 14:18:36 -0600

    change package manager in github action

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <!-- whole page -->
+  <div style="display: flex; align-items: center; justify-content: center; flex-flow: column;">
+    <!-- filters -->
+    <div style="display: flex;">
+      left
+    </div>
+    <!-- product list -->
+    <div style="display: flex;">
+      right
+    </div>
   </div>
 </template>
